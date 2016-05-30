4937400c5e83cc1bb71d96c759c27882c0e4b757
AlicanAkkus <alican.akkus94@gmail.com>
2016-05-30 13:27:08 +0300

    null check for fields

--- src/com/wora/stringify/Stringify.java
+++ src/com/wora/stringify/Stringify.java
@@ -1,37 +1,53 @@
 package com.wora.stringify;
-import java.lang.reflect.Field;
 
+import java.lang.reflect.Field;
 import com.wora.enums.ReturnType;
 
-public class Stringify {
 
+
+/** 
+ * The DataClass program implement reflection api and print all variable of object
+ * @author wora - alican akkus
+ * @since 14-03-2016
+ * @version 1.0
+ * {
+ * 	@link https://github.com/AlicanAkkus/Data-Class-Java
+ * }
+ * 
+ * */
+
+public class Stringify {
+	
+	
+	/**
+	 * @param o to printed
+	 * @param type of return type such as string, xml, json. not yet implemented
+	 * @return all class member returned (name : value) pair
+	 * */
 	public static synchronized String toString(Object o, ReturnType type) {
 
 		// Book book = new Book("ASD", 11);
 
+		StringBuilder builder = new StringBuilder("\n*******************BEGIN*******************\n");
+		builder.append("{\n");
 		try {
 			Class<?> c = o.getClass();
 
 			Field fields[] = c.getDeclaredFields();
-			StringBuilder builder = new StringBuilder("\n*******************BEGIN*******************\n");
-			builder.append("{\n");
-			boolean complexObject = false;
-			//
-			// if (type == RETURN_TYPE.STRING) {
-			// builder.append("{\n");
-			// }
 
 			for (Field f : fields) {
 				Field declaredField = c.getDeclaredField(f.getName());
 				declaredField.setAccessible(true);// for private variable
 
 				builder.append("\t").append(declaredField.getName()).append(" : ");
-				builder.append(declaredField.get(o).toString()).append("\n");
+				if(declaredField.get(o) != null){
+					builder.append(declaredField.get(o).toString()).append("\n");
+				}else{
+					builder.append("Unknow").append("\n");
+				}
 
 			}
 
-			if (complexObject)
-				builder.append("\t");
 
 			builder.append("}");
 			builder.append("\n####################END####################\n");
@@ -41,8 +57,12 @@ public class Stringify {
 
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "";
+			return builder.toString();
 		}
 
+	}
+	
+	public static String toString(Object o) {
+		return toString(o,ReturnType.STRING);
 	}
 }
